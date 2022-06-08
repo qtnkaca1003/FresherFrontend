129c9153ec167ebe46bee1425f5e2006d376af81
nhanqt <nhanqt.runsystem.net>
2022-06-08 16:57:45 +0700

    add component storybook

--- manager/stories/Atoms/Avatar/Avatar.stories.tsx
+++ manager/stories/Atoms/Avatar/Avatar.stories.tsx
@@ -1,17 +1,20 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
 import CAvatar from "./avatar";
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Atoms/Avatar",
   component: CAvatar,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
   argTypes: {
     backgroundColor: { control: "color" },
+    m: { description: "string" },
+    size: {
+      description: `"2xs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "full"`,
+    },
+    src: { description: "string" },
   },
 } as ComponentMeta<typeof CAvatar>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof CAvatar> = (args) => (
   <CAvatar {...args} />
 );
